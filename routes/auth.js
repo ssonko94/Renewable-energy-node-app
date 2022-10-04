f5e302652881d03a3d9b5966d11dbb9586c24aa1
Jimmy@devs <ssonkojimmy@gmail.com>
2022-10-04 21:32:32 +0300

    (error-handling, post-signup): Add more validation logic for the signup rote
    -Add more checking to ensure we only accept valid data format for the signup data

---
+++ routes/auth.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const { body } = require("express-validator");
+
+const User = require("../models/user");
+
+const authController = require("../controllers/auth");
+
+const router = express.Router();
+
+router.post(
+  "/api/signup",
+  [
+    body("firstName").trim().isLength({ min: 3 }).not().isEmpty(),
+    body("lastName").trim().isLength({ min: 3 }).not().isEmpty(),
+    body("email")
+      .isEmail()
+      .withMessage("Please enter a valid email address")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            return Promise.reject("E-mail already exists");
+          }
+        });
+      })
+      .normalizeEmail(),
+    body("password").trim().isLength({ min: 5 }),
+    body("rights").trim().isLength({ min: 3 }).not().isEmpty(),
+  ],
+  authController.signup
+);
+
+router.post("/api/login", authController.login);
+
+module.exports = router;
