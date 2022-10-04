d924af9548e3ac206830f28d518d150173ea4810
Jimmy@devs <ssonkojimmy@gmail.com>
2022-10-04 19:47:15 +0300

    (server-side, request validation): Add validation for the post signup request
    -With the express validator middleware validate all incoming requests to ensure we get correct data schema

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,4 +1,13 @@
+const { validationResult } = require("express-validator");
+
 exports.postSignup = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Validation, failed entered data is incorrect",
+      errors: errors.array(),
+    });
+  }
   const body = req.body;
   console.log(body);
   res.status(201).json({ message: "Succefully created an account" });
