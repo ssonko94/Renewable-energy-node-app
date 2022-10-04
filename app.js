a9a8d4307113a6fd0f5cf4d0d4152cb5b87ad4a5
Jimmy@devs <ssonkojimmy@gmail.com>
2022-10-04 05:56:27 +0300

    (server, middleware): In the entry file start the server
    -Create a server using the express middleware and add some essential middleware.
    
    -Add middleware to set headers that will prevent getting CORS errors from the client side.
    
    -Add our route as middleware and body parser to parse our incoming request bodies

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const signupRoutes = require("./routes/signup");
+const PORT = 8080;
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, PUT, POST, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use(signupRoutes);
+app.listen(PORT);
