49f09a5b14e5a12f590fd77515564d1c5d79ba51
Jimmy@devs <ssonkojimmy@gmail.com>
2022-10-04 21:27:16 +0300

    (Fix, error-handling): Add error handling middleware
    -Add a middleware function that will be in charge of errors thrown in the application as this will prevent crushing of the application

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const signupRoutes = require("./routes/signup");
-const PORT = 8080;
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+const authRoutes = require("./routes/auth");
+const PORT = 8080;
 const DATABASEURI =
   "mongodb+srv://MubiruIsaac:YJ9VPGQFRy2qEb5i@renewableenergy.tur0voc.mongodb.net/renewableEnergy?retryWrites=true&w=majority";
 
@@ -20,7 +20,18 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(signupRoutes);
+app.use(authRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+
+  res.status(status).json({ message: message, data: data });
+});
+
 mongoose
   .connect(DATABASEURI)
   .then((result) => {
