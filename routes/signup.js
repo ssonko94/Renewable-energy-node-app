005e3bd6f854ec3402511460a35b346ce30687fc
Jimmy@devs <ssonkojimmy@gmail.com>
2022-10-04 19:44:12 +0300

    (fix, deprecated-module-error): fix error showing deprecated module
    -Update the module to use current supported import syntax so that the error in the console is fixed.

--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const { body } = require("express-validator");
 
 const signupController = require("../controllers/signup");
 
 const router = express.Router();
 
-router.post("/api/signup", signupController.postSignup);
+router.post(
+  "/api/signup",
+  [body("username").trim().isLength({ min: 5 })],
+  signupController.postSignup
+);
 
 module.exports = router;
