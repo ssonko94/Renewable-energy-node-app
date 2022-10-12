ac9480646bde5345395952d7c2245819b5cf5cac
Jimmy@devs <ssonkojimmy@gmail.com>
2022-10-12 08:57:53 +0300

    (Cors, express-cors): Use express cors middleware to fix cors issues
    -Refactor code to use cors middles ware instead of manually setting specific headers

--- app.js
+++ app.js
@@ -1,36 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
-const authRoutes = require("./routes/auth");
+const authRoutes = require("./routes/authRoute");
+const { errorLogger, errorResponder } = require("./utils/error");
+
 const PORT = 8080;
 const DATABASEURI =
   "mongodb+srv://MubiruIsaac:YJ9VPGQFRy2qEb5i@renewableenergy.tur0voc.mongodb.net/renewableEnergy?retryWrites=true&w=majority";
 
 const app = express();
 app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, PUT, POST, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+app.use(cors());
 
 app.use(authRoutes);
 
-app.use((error, req, res, next) => {
-  console.log(error);
-
-  const status = error.statusCode || 500;
-  const message = error.message;
-  const data = error.data;
-
-  res.status(status).json({ message: message, data: data });
-});
+app.use(errorLogger);
+app.use(errorResponder);
 
 mongoose
   .connect(DATABASEURI)
