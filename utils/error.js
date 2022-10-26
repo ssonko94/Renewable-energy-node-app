a2013741f4aa15fc421b712e775196db4d14e927
Jimmy@devs <ssonkojimmy@gmail.com>
2022-10-26 17:27:34 +0300

    (Error-handling): Add a error class to create custom errors.
    -Add an error class that inherits from the error object to have a more customised message

--- utils/error.js
+++ utils/error.js
@@ -9,24 +9,4 @@ const errorResponder = (error, req, res, next) => {
     .send(JSON.stringify({ message: error.message, code: error.statusCode }));
 };
 
-const errorWrapper = (func) => async (req, res, next) => {
-  try {
-    await func(req, res);
-  } catch (error) {
-    return next(error);
-  }
-};
-
-const createError = ({ error, message, code, type }) => {
-  const err = new Error(error);
-  err.type = type;
-  err.statusCode = code;
-  err.message = message;
-  return err;
-};
-
-module.exports = {
-  errorResponder,
-  errorWrapper,
-  createError,
-};
+module.exports = errorResponder;
---
+++ utils/errorWrapper.js
@@ -0,0 +1,9 @@
+const errorWrapper = (func) => async (req, res, next) => {
+  try {
+    await func(req, res, next);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+module.exports = errorWrapper;
