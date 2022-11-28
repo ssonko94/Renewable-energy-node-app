230e168fb26f53bc5f76a682f16edfd0f3fbbb4b
Jimmy@devs <ssonkojimmy@gmail.com>
2022-11-28 13:16:34 +0300

    (Setup, node-mailer): Setup configuration for node mailer
    - Install node mailer and create a configuration file for the confirm signup logic.
    
    -Add a controller for the confirm signup route to activate a user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const errorWrapper = require("../utils/errorWrapper");
 const AppError = require("../utils/baseError");
 const signUpValidator = require("../utils/signupSchema");
+const { sendConfirmationEmail } = require("../nodemailer.config");
 
 const User = require("../models/user");
 
@@ -17,34 +19,39 @@ const signupFunc = async (req, res, next) => {
   const { error } = await signUpValidator(req.body);
 
   if (error) {
-    next(new AppError(error.message, 422));
+    return next(new AppError(error.message, 422));
   }
 
   if (!hashedPassword) {
-    next(new AppError("Failed to encrypt password", 500));
+    return next(new AppError("Failed to encrypt password", 500));
   }
 
-  const user = User.create({
+  const token = jwt.sign({ email: req.body.email }, process.env.JWT_SECRET);
+
+  const user = await User.create({
     firstName: firstName,
     lastName: lastName,
     email: email,
     password: hashedPassword,
     rights: rights,
+    confirmationCode: token,
   });
 
   if (!user) {
-    next(new AppError("Failed to create user", 401));
+    return next(new AppError("Failed to create user", 401));
   }
   const result = await user.save();
 
   if (!result) {
-    next(new AppError("Failed to save user", 401));
+    return next(new AppError("Failed to save user", 401));
   }
 
   res.status(201).json({
     message: "User signed up successfully",
     userId: result._id.toString(),
   });
+
+  sendConfirmationEmail(user.firstName, user.email, user.confirmationCode);
 };
 
 const loginFunc = async (req, res, next) => {
@@ -55,23 +62,57 @@ const loginFunc = async (req, res, next) => {
 
   const user = await User.findOne({ email: email });
   if (!user) {
-    next(new AppError("No user found with this email", 401));
+    return next(new AppError("No user found with this email", 401));
   }
   loadedUser = user;
+  if (user.status !== "Active") {
+    return next(new Error("Pending Account. Please Verify Your Email!", 401));
+  }
   const isEqual = await bcrypt.compare(password, user.password);
   if (!isEqual) {
-    next(new AppError("Wrong password provided", 401));
+    return next(new AppError("Wrong password provided", 401));
   }
 
   const token = jwt.sign(
-    { email: loadedUser.email, userId: loadedUser._id.toString() },
-    "someofthebiggestmostsecuretokensyouwillfindontheinternet",
+    { userId: loadedUser._id.toString() },
+    process.env.JWT_SECRET,
     { expiresIn: "1hr" }
   );
 
-  res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+  res.status(200).json({
+    token: token,
+    userId: loadedUser._id.toString(),
+    expiresIn: new Date().setMilliseconds(3600000),
+  });
+};
+
+const confirmUserFunc = async (req, res, next) => {
+  const user = await User.findOne({
+    confirmationCode: req.params.confirmationCode,
+  });
+
+  if (!user) {
+    return next(new AppError("User Not found.", 401));
+  }
+
+  user.status = "Active";
+
+  const result = await user.save();
+
+  if (!result) {
+    return next(new AppError(result.message, 401));
+  } else {
+    return res
+      .status(200)
+      .set("Content-Type", "text/html")
+      .send(
+        `<h1>${user.firstName}, you have successfully been signed up. You can now go back and login</h1>`
+      );
+  }
 };
 
 exports.signup = errorWrapper(signupFunc);
 
 exports.login = errorWrapper(loginFunc);
+
+exports.confirmUser = errorWrapper(confirmUserFunc);
---
+++ nodemailer.config.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+
+const user = process.env.SENDER_EMAIL;
+const pass = process.env.SENDER_PASSWORD;
+
+const transport = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user,
+    pass,
+  },
+});
+
+module.exports.sendConfirmationEmail = (name, email, confirmationCode) => {
+  console.log("Check");
+  transport
+    .sendMail({
+      from: user,
+      to: email,
+      subject: "Please confirm your account",
+      html: `<h1>Email Confirmation</h1>
+          <h2>Hello ${name}</h2>
+          <p>Thank you for subscribing. Please confirm your email by clicking on the following link</p>
+          <a href=http://localhost:8080/api/auth/confirm/${confirmationCode}> Click here</a>
+          </div>`,
+    })
+    .catch((err) => console.log(err));
+};
